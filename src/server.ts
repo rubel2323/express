ba3168b8ce5c7e92aa07b30fe187a0335131f97f
Rubel <mr.adcadre41@gmail.com>
2026-06-17 07:47:36 +0600

    node js type added  in ExpressJs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/user", (req, res) => {
-  res.send("Hello World!");
+app.get("/user", (req: Request, res: Response) => {
+  // res.send("Hello World!");
+  res.status(200).json({
+    message: "This is Express server",
+    author: "Next Level",
+    version: "2.0.12",
+  });
 });
 
 app.listen(port, () => {
